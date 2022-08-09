48adeec908b34108830069dda68f34f1e320972e
Hind <hind.nayhi@gmail.com>
2022-08-09 13:38:41 -0600

    Linted + Added comments to explain code logic.

--- express_server.js
+++ express_server.js
@@ -1,65 +1,53 @@
 const express = require('express');
 const app = express();
-const PORT = 8080; // default port 8080
+const PORT = 8080; //Default port 8080
 
-app.set('view engine', 'ejs');
+app.set('view engine', 'ejs'); //To use EJS templates.
 
 const urlDatabase = {
   'b2xVn2': 'http://www.lighthouselabs.ca',
   '9sm5xK': 'http://www.google.com'
 };
 
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true })); //Middleware that translates the request body.
 
-app.post("/urls/:id/edit", (req, res) => {
+app.post('/urls/:id/edit', (req, res) => { //Setup a route for the Edit button.
   urlDatabase[req.params.id] = req.body.longURL;
-  res.redirect('/urls');
+  res.redirect('/urls'); //Redirection to /urls after submission.
 });
 
-app.post("/urls/:id/delete", (req, res) => {
+app.post('/urls/:id/delete', (req, res) => { //Setup a route for the Delete button.
   delete urlDatabase[req.params.id];
-  res.redirect(`/urls`);
+  res.redirect(`/urls`); //Redirection to /urls after clicking on the Delete button.
 });
 
-app.post("/urls", (req, res) => {
+app.post('/urls', (req, res) => {
   const id = generateRandomString();
-  urlDatabase[id] = req.body.longURL;
-  res.redirect(`/urls/${id}`);
+  urlDatabase[id] = req.body.longURL; //Save the id-longURL key-value pair to urlDatabase.
+  res.redirect(`/urls/${id}`); //Redirection from /urls to /urls/:id
 });
 
-app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+app.get('/urls/new', (req, res) => { //Setup a route to show the Form/ render urls_new.ejs
+  res.render('urls_new');
 });
 
-app.get("/urls/:id", (req, res) => {
+app.get('/urls/:id', (req, res) => { //Ship the object templateVars off to the template urls_show.ejs
   const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
-  res.render("urls_show", templateVars);
+  res.render('urls_show', templateVars);
 });
 
-app.get("/u/:id", (req, res) => {
+app.get('/u/:id', (req, res) => { //Handles shortURL (id) requests.
   const longURL = urlDatabase[req.params.id];
-  res.redirect(longURL);
+  res.redirect(longURL); //Redirection from /u/:id to its longURL.
 });
 
-app.get('/urls', (req, res) => {
+app.get('/urls', (req, res) => { //Link the object templateVars to the template urls_index.ejs
   const templateVars = { urls: urlDatabase };
   res.render('urls_index', templateVars);
 });
 
-app.get('/hello', (req, res) => {
-  res.send('<html><body>Hello <b>World</b></body></html>\n');
-});
-
-app.get('/urls.json', (req, res) => {
-  res.json(urlDatabase);
-});
-
-app.get('/', (req, res) => {
-  res.send('Hello!');
-});
-
 app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}!`);
+  console.log(`TinyApp is listening on port ${PORT}!`);
 });
 
 const generateRandomString = function() {
